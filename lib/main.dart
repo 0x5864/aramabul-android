36cbd6ddc0813a31962385feeea86e00db856151
Metin Tuncgenc <mailin@example.com>
2026-05-07 18:20:09 +0300

    fix: show distance element again, icon bg transparent

--- lib/main.dart
+++ lib/main.dart
@@ -759,7 +759,8 @@ class _HomeWebViewPageState extends State<HomeWebViewPage> {
           '.istanbul-pagination-button { background: #deab6d !important; border-color: #deab6d !important; color: #000 !important; }' +
           '.istanbul-pagination-current { background: #497676 !important; border-color: #497676 !important; color: #fff !important; }' +
           '.istanbul-results-mode { display: none !important; }' +
-          '.istanbul-venue-distance { display: none !important; }' +
+          '.istanbul-venue-distance { background: transparent !important; }' +
+          '.istanbul-venue-distance::before { background-color: transparent !important; }' +
           '.istanbul-favorite-button { background: #deab6d !important; border-color: #deab6d !important; color: #000 !important; }' +
           '#favoritesTitle { color: #ffffff !important; }' +
           '.istanbul-results-head h2 { color: #ffffff !important; }';
